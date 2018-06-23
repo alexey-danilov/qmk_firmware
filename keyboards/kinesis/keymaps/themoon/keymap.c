2bb6f20ceb00b2197d24a1133940aa48b82d7f68
Alexey Danilov <alexey.danilov@movedin.co>
2018-06-23 13:12:53 +0300

    Initial keymap for Kinesis Advantage2 with palm keys

--- keyboards/kinesis/keymaps/themoon/keymap.c
+++ keyboards/kinesis/keymaps/themoon/keymap.c
@@ -3,29 +3,28 @@
 #define __________ KC_TRNS
 #define XXXXXXX KC_NO
 
-// mac layers: ins + int4 (before first plug in)
+// mac layers: 5 + f5
 #define _MAC 0
 #define _PALM 1
 #define _MAC_CMD_ESC 2
 #define _MAC_CMD_SPACE 3
 #define _MAC_DEL 4
 
-// win layers: int + int5
+// win layers: 5 + f7
 #define _WIN 5
 
 enum kinesis_keycodes {
   _M_CMD_ESC = LT(_MAC_CMD_ESC, KC_ESC),
   _M_CMD_SPACE = LT(_MAC_CMD_SPACE, KC_SPC),
   _M_DEL = LT(_MAC_DEL, KC_BSPC),
-  _P = MO(_PALM),
-
-  SHIFT_TAB = SFT_T(KC_TAB),
+  _P1 = MO(_PALM),
+  _P2 = LT(_PALM, KC_F14),
 
   ALT_SLASH = MT(MOD_LALT, KC_SLSH),
   ALT_BSLASH = MT(MOD_RALT, KC_BSLS),
 
   CTRL_DEL = MT(MOD_LCTL, KC_DEL),
-  CTRL_F17 = MT(MOD_RCTL, KC_F17),
+  CTRL_F13 = MT(MOD_RCTL, KC_F13),
 };
 
 enum holding_keycodes {
@@ -78,26 +77,26 @@ enum {
 * Keymap: Default Layer in Qwerty
 *
 * ,-------------------------------------------------------------------------------------------------------------------.
-* | Power  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |  FN0 |Program |
+* | Power  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | Mute | Vol- | Vol+ | FN0? |Program |
 * |--------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------|
 * | =+     |  1!  |  2@  |  3#  |  4$  |  5%  |                           |  6^  |  7&  |  8*  |  9(  |  0)  | -_     |
 * |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|
-* | `~     |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | Mail   |
+* | `~     |   Q  |   W  |   E  |   R  |   T  |                           |   Y  |   U  |   I  |   O  |   P  | F16   |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | CAPS   |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | F20    |
+* | CAPS   |   A  |   S  |   D  |   F  |   G  |                           |   H  |   J  |   K  |   L  |  ;:  | F17    |
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
-* | F19    |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  Up  |  .>  |  '"  | F21    |
+* | Mail   |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  Up  |  .>  |  '"  | F18    |
 * `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
-*          |Insert|  [{  |  ,<  |  ]}  |                                         | Left | Down | Right| M2   |
+*          |Insert|  [{  |  ,<  |  ]}  |                                         | Left | Down | Right| F15  |
 *          `---------------------------'                                         `---------------------------'
 *                            .-------------------------.         ,-------------------------.
 *                            | LShift+LAlt/BkSp |Trash |         | Sleep| LShift+LAlt/BkSp |
 *                            `-----------|------|------|         |------+------+-----------`
 *                                 |      |      | Alt//|         | Alt/\|      |      |
 *                                 | LCMD/|Shift/|------|         |------|Shift/|RCMD/ |
-*                                 | ESC  |Enter |Ctrl/Del|     |Ctrl/F17|Tab   |SPACE |
+*                                 | ESC  |Enter |Ctrl/Del|     |Ctrl/F13|Tab   |SPACE |
 *                                 `--------------------'         `--------------------'
-*                            PALM                                                      PALM
+*                             PALM                                                     PALM/F14
 */
 
 // base layer
@@ -108,27 +107,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5,
            KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T,
            KC_CAPSLOCK,KC_A, KC_S, KC_D, KC_F, KC_G,
-           KC_F19 ,KC_Z, KC_X, KC_C, KC_V, KC_B,
+           M(MAIL) ,KC_Z, KC_X, KC_C, KC_V, KC_B,
                  KC_INS, KC_LBRC, KC_COMM, KC_RBRC,
                                            // left thumb keys
 			                                    _M_DEL,LGUI(KC_BSPC),
                                                    ALT_SLASH,
                            _M_CMD_ESC, KC_SFTENT, CTRL_DEL,
                                      // left palm key
-			                         _P,
+			                         _P1,
     // right side
-    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SLCK  ,KC_PAUS, KC_FN0, RESET,
+    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC__MUTE ,KC__VOLDOWN  ,KC__VOLUP, KC_FN0, RESET,
 	KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-	KC_Y, KC_U, KC_I, KC_O, KC_P, M(MAIL),
-	KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_F20,
-	KC_N, KC_M, KC_UP, KC_DOT, KC_QUOT, KC_F21,
-	KC_LEFT, KC_DOWN, KC_RGHT, KC_MS_BTN2,
+	KC_Y, KC_U, KC_I, KC_O, KC_P, KC_F16,
+	KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_F17,
+	KC_N, KC_M, KC_UP, KC_DOT, KC_QUOT, KC_F18,
+	KC_LEFT, KC_DOWN, KC_RGHT, KC_F15,
            // right thumb keys
            M(SLEEP),_M_DEL,
            ALT_BSLASH,
-           CTRL_F17, SHIFT_TAB, _M_CMD_SPACE,
+           CTRL_F13, SFT_T(KC_TAB), _M_CMD_SPACE,
                                     // right palm key
-                                    _P
+                                    _P2
     ),
 
 [_MAC_CMD_ESC] = KEYMAP(
@@ -148,10 +147,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          CTRL_H,  __________,  MOD_K,  __________,  __________,  __________,
          MOD_N,  CTRL_M,  MOD_UP,  CTRL_DOT ,  __________,  __________,
                    MOD_LEFT,  MOD_DOWN,  MOD_RIGHT, __________,
-         KC_F15,  ALT_BSPC,
-         __________,
-         KC_F17,  CTRL_TAB,  MOD_SPACE,
-                             __________
+         KC_F2,  ALT_BSPC,
+         KC_BSLS,
+         KC_F13,  CTRL_TAB,  MOD_SPACE,
+                             KC_F14
     ),
 
 [_MAC_CMD_SPACE] = KEYMAP(
@@ -161,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          __________,  __________,  MOD_S,  MOD_D,  MOD_F,  MOD_G,
          __________,  __________,  MOD_X,  MOD_C,  MOD_V,  MOD_B,
                    __________,  MOD_LBRC,  CTRL_COMMA,  MOD_RBRC,
-                             ALT_BSPC,  __________,
+                             ALT_BSPC,  KC_F1,
                                         KC_Z,
                     MOD_ESC, MOD_ENTER, LSFT(KC_Z),
                                      __________,
@@ -229,11 +228,11 @@ __________,  __________,  __________,  __________,  __________,  __________, ___
          __________,  __________,  __________,  __________,  __________,  __________,
          __________,  __________,  __________,  __________,  __________,  __________,
          __________,  __________,  __________,  __________,  __________,  __________,
-                   __________,  KC__VOLDOWN,  KC__MUTE,  KC__VOLUP,
-                             KC_F1,  KC_F2,
-                                     KC_F3,
-                      KC_F4, KC_F5,  KC_F6,
-                                     _P,
+                   __________,  __________,  __________,  __________,
+                             __________,  __________,
+                                     __________,
+                      __________, __________,  __________,
+                                     _P1,
          __________,  __________,  __________,  __________,  __________,  __________, __________, __________, __________,
          __________,  __________,  __________,  __________,  __________,  __________,
          __________,  __________,  __________,  __________,  __________,  __________,
@@ -243,7 +242,7 @@ __________,  __________,  __________,  __________,  __________,  __________, ___
          __________,  __________,
          __________,
          __________,  __________,  __________,
-                             _P
+                             _P2
     ),
 };
 
@@ -425,9 +424,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_PGDN: {return no_meh(KC_PGDN, record->event.pressed); }
         case KC_HOME: {return no_meh(KC_HOME, record->event.pressed); }
         case KC_END: {return no_meh(KC_END, record->event.pressed); }
-        case KC__VOLDOWN: {return no_meh(KC__VOLDOWN, record->event.pressed); }
-        case KC__MUTE: {return no_meh(KC__MUTE, record->event.pressed); }
-        case KC__VOLUP: {return no_meh(KC__VOLUP, record->event.pressed); }
 
         // 140 ms
         case MOD_LEFT: {return ___if_held_140___add_shift(KC_LEFT, record->event.pressed); }
