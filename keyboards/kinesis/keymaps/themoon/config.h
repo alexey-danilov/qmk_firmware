e52d925ef84be75d858a154c6ee042efc2f2a1a6
Alexey Danilov <oleksii.danilov@gmail.com>
2019-01-10 17:09:50 +0200

    Adjusting led power off dance and tapping term

--- keyboards/kinesis/keymaps/themoon/config.h
+++ keyboards/kinesis/keymaps/themoon/config.h
@@ -1,8 +1,8 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#define AUTO_SHIFT_TIMEOUT 160
-#define TAPPING_TERM 160
+#define AUTO_SHIFT_TIMEOUT 155
+#define TAPPING_TERM 155
 
 #include "../../config.h"
 #endif
