2faac820202738c2e95775fcab0d230c257e580e
Alexey_Danilov_home <oleksii.danilov@gmail.com>
2019-01-10 20:10:41 +0200

    Decrease tapping term

--- keyboards/kinesis/keymaps/themoon/config.h
+++ keyboards/kinesis/keymaps/themoon/config.h
@@ -1,8 +1,9 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#define AUTO_SHIFT_TIMEOUT 155
-#define TAPPING_TERM 155
+#define AUTO_SHIFT_TIMEOUT 150
+#define TAPPING_TERM 150
+#define RETRO_TAPPING
 
 #include "../../config.h"
 #endif
