d99672d52c046b4e96d84bba001b97cca8c56750
Alexey Danilov <oleksii.danilov@gmail.com>
2019-01-16 12:49:06 +0200

    tapping term -> 160

--- keyboards/kinesis/keymaps/themoon/config.h
+++ keyboards/kinesis/keymaps/themoon/config.h
@@ -1,8 +1,12 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#define AUTO_SHIFT_TIMEOUT 175
-#define TAPPING_TERM 175
+#define AUTO_SHIFT_TIMEOUT 160
+#define TAPPING_TERM 160
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
 
 #include "../../config.h"
 #endif
