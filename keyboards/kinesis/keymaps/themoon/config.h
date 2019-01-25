b9af10c8fc018e22ea7f9636e002bb85623ec1e1
Alexey_Danilov_home <oleksii.danilov@gmail.com>
2019-01-25 08:26:04 +0200

    Experimenting with lower tap term values

--- keyboards/kinesis/keymaps/themoon/config.h
+++ keyboards/kinesis/keymaps/themoon/config.h
@@ -1,11 +1,12 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#define AUTO_SHIFT_TIMEOUT 145
-#define TAPPING_TERM 145
+#define AUTO_SHIFT_TIMEOUT 150
+#define TAPPING_TERM 70
 
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
 #define RETRO_TAPPING
 
 #include "../../config.h"
