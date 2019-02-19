ae9bc8afa9cf6b8fd69ccd6a745632863a349a7d
Alexey_Danilov_home <oleksii.danilov@gmail.com>
2019-02-19 17:08:27 +0200

    Increased delays for shutdown on win

--- keyboards/kinesis/keymaps/themoon/keymap.c
+++ keyboards/kinesis/keymaps/themoon/keymap.c
@@ -659,7 +659,7 @@ void rest_finished (qk_tap_dance_state_t *state, void *user_data) {
             down(KC_LGUI); down(KC_LCTL); down(KC_LALT); SEND_STRING(SS_DOWN(X_POWER) SS_UP(X_POWER)); up(KC_LALT); up(KC_LCTL); up(KC_LGUI); break;
          }
          if (isWin) {
-            with_1mod(KC_X, KC_LGUI); _delay_ms(300); led_blue_off(); key_code(KC_U); _delay_ms(300); led_green_off(); _delay_ms(300); led_yellow_off(); _delay_ms(300); led_red_off(); _delay_ms(300); key_code(KC_U); _delay_ms(300); break;
+            with_1mod(KC_X, KC_LGUI); _delay_ms(700); led_blue_off(); key_code(KC_U); _delay_ms(700); led_green_off(); _delay_ms(700); led_yellow_off(); _delay_ms(700); led_red_off(); _delay_ms(700); key_code(KC_U); _delay_ms(700); break;
          }
       default: break;
     }
