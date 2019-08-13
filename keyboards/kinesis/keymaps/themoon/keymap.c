cae502d07cde9559ea48f5e7c737333688b45677
Alexey Danilov <oleksii.danilov@gmail.com>
2019-08-13 16:55:42 +0300

    removed G(KC_Q) on mac right palm

--- keyboards/kinesis/keymaps/themoon/keymap.c
+++ keyboards/kinesis/keymaps/themoon/keymap.c
@@ -1383,7 +1383,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          HYPR(KC_F9), HYPR(KC_A), HYPR(KC_S), HYPR(KC_D), HYPR(KC_F), HYPR(KC_G),
          HYPR(KC_F10), HYPR(KC_Z), HYPR(KC_X), HYPR(KC_C), HYPR(KC_V), HYPR(KC_B),
                  HYPR(KC_GRV), KC_MRWD, KC_MPLY, KC_MFFD,
-                                   LGUI(KC_Q), HYPR(KC_F15),
+                                   HYPR(KC_BSPC), HYPR(KC_F15),
                                                HYPR(KC_F17),
                  LGUI(KC_Z), LGUI(LSFT(KC_Z)), HYPR(KC_DEL),
                                               HYPR(F5_PALM),
