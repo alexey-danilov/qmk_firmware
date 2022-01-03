dd9f88f483ca33723f448b9ddcacb3eedf408832
Alexey Danilov <oleksii.danilov@games24x7.com>
2022-01-03 16:34:01 +0200

    adjusted mappings of failsafe layers

--- keyboards/kinesis/keymaps/themoon/keymap.c
+++ keyboards/kinesis/keymaps/themoon/keymap.c
@@ -2866,26 +2866,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_INS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,
                 KC_PGUP, KC_1, KC_2, KC_3, KC_4, KC_5,
                 KC_PGDN, KC_Q, KC_W, KC_E, KC_R, KC_T,
-                KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,
-                KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
+                KC_LСTL, KC_A, KC_S, KC_D, KC_F, KC_G,
+                KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,
                     KC_GRV, KC_LBRC, KC_COMM, KC_RBRC,
                                                     // left thumb keys
-    		                                           KC_BSPC, KC_BSLS,
+    		                                            KC_BSPC, KC_DEL,
                                                                 KC_SLSH,
-                                               KC_ESC, KC_ENTER, KC_DEL,
+                                               KC_ESC, KC_LSFT, KC_BSLS,
                                                        // left palm key
               	                                                KC_LGUI,
                   // right side
                 KC_F9, KC_F10, KC_F11, KC_F12, _, _, _, _, TD(MAC_EXIT_FAILSAFE),
               	KC_6, KC_7, KC_8, KC_9, KC_0, KC_END,
               	KC_Y, KC_U, KC_I, KC_O, KC_P, KC_HOME,
-              	KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RALT,
-              	KC_N, KC_M, KC_UP, KC_DOT, KC_QUOT, KC_RSFT,
+              	KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RCTL,
+              	KC_N, KC_M, KC_UP, KC_DOT, KC_QUOT, KC_RALT,
               	KC_LEFT, KC_DOWN, KC_RGHT, KC_NUBS,
                          // right thumb keys
-                         KC_RCTL, KC_BSPC,
-                         KC_EQL,
-                         KC_MINS, KC_TAB, KC_SPC,
+                         KC_EQL, KC_MINS,
+                         KC_ENTER,
+                         KC_TAB, KC_RSFT, KC_SPC,
                          // right palm key
                          KC_RGUI
                   ),
@@ -2895,26 +2895,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_INS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,
                KC_PGUP, KC_1, KC_2, KC_3, KC_4, KC_5,
                KC_PGDN, KC_Q, KC_W, KC_E, KC_R, KC_T,
-               KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,
-               KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,
+               KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G,
+               KC_LALT, KC_Z, KC_X, KC_C, KC_V, KC_B,
                    KC_GRV, KC_LBRC, KC_COMM, KC_RBRC,
                                                     // left thumb keys
-                                                      KC_BSPC, KC_BSLS,
+    		                                           KC_BSPC, KC_DEL,
                                                                KC_SLSH,
-                                              KC_ESC, KC_ENTER, KC_DEL,
+                                              KC_ESC, KC_LSFT, KC_BSLS,
                                                        // left palm key
                              	                               KC_LCTL,
               // right side
               KC_F9, KC_F10, KC_F11, KC_F12, _, _, _, _, TD(PC_EXIT_FAILSAFE),
-              KC_6, KC_7, KC_8, KC_9, KC_0, KC_END,
-              KC_Y, KC_U, KC_I, KC_O, KC_P, KC_HOME,
-              KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RALT,
-              KC_N, KC_M, KC_UP, KC_DOT, KC_QUOT, KC_RSFT,
+              KC_PGUP, KC_7, KC_8, KC_9, KC_0, KC_END,
+              KC_PGDN, KC_U, KC_I, KC_O, KC_P, KC_HOME,
+              KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RGUI,
+              KC_N, KC_M, KC_UP, KC_DOT, KC_QUOT, KC_RALT,
               KC_LEFT, KC_DOWN, KC_RGHT, KC_NUBS,
                        // right thumb keys
-                       KC_LGUI, KC_BSPC,
-                       KC_EQL,
-                       KC_MINS, KC_TAB, KC_SPC,
+                       KC_EQL, KC_MINS,
+                       KC_ENTER,
+                       KC_TAB, KC_RSFT, KC_SPC,
                        // right palm key
                        KC_RCTL
               ),
